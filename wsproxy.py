34c52d09e6e4c44725539b8dd861bb7553f79720
Andley S <39422775+Andley302@users.noreply.github.com>
2021-08-30 13:11:48 -0300

    Update wsproxy.py

--- wsproxy.py
+++ wsproxy.py
@@ -12,7 +12,7 @@ except:
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = "127.0.0.1:22"
-RESPONSE = 'HTTP/1.1 101 SSPLUS \r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 OneVPS \r\n\r\n'
  
 class Server(threading.Thread):
     def __init__(self, host, port):
